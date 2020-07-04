59d8e3e2e872f29dafcd174c52e7803d366971c9
shalaga44 <mohamedmoawia4@gmail.com>
2020-07-04 20:39:14 +0200

    validate Sign Up inputs

--- Android-ChatApp/app/src/main/java/com/shalaga44/chatapp/SignupActivity.kt
+++ Android-ChatApp/app/src/main/java/com/shalaga44/chatapp/SignupActivity.kt
@@ -2,6 +2,7 @@ package com.shalaga44.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import com.shalaga44.chatapp.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
@@ -14,11 +15,77 @@ class SignupActivity : AppCompatActivity() {
         setContentView(binding.root)
         attachViewsOnClickListener()
 
+
+    }
+
+    private fun validatePassword(): Boolean {
+        val passwordInput = binding.passwordInput.editText?.text?.trim()
+        return when {
+            passwordInput.isNullOrEmpty() -> {
+                binding.passwordInput.error = "Field can't be empty"
+                false
+            }
+            passwordInput.length < 7 -> {
+                binding.passwordInput.error = "Password is weak"
+                false
+            }
+            else -> {
+                binding.passwordInput.error = ""
+                true
+            }
+        }
+
+    }
+
+    private fun validateInput(): Boolean {
+        validateUserName()
+        validateEmail()
+        validatePassword()
+
+
+        return false
+    }
+
+
+    private fun validateEmail(): Boolean {
+        val emailInput = binding.emailInput.editText?.text?.trim()
+        if (emailInput.isNullOrEmpty()) {
+            binding.emailInput.error = "Field can't be empty"
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            binding.emailInput.error = "Not valid email"
+            return false
+        } else {
+            binding.emailInput.error = ""
+            return true
+        }
+
+
+    }
+
+
+    private fun validateUserName(): Boolean {
+        val usernamelInput = binding.usernameInput.editText?.text?.trim()
+        return when {
+            usernamelInput.isNullOrEmpty() -> {
+                binding.usernameInput.error = "Field can't be empty"
+                false
+            }
+            usernamelInput.length > 15 -> {
+                binding.usernameInput.error = "username too long"
+                false
+            }
+            else -> {
+                binding.usernameInput.error = ""
+                true
+            }
+        }
+
     }
 
     private fun attachViewsOnClickListener() {
         binding.confirm.setOnClickListener {
-
+            validateInput()
         }
     }
 }
