097c3074ac2815b4bd1ecb51fea7da71d813ae97
shalaga44 <mohamedmoawia4@gmail.com>
2020-07-06 13:00:41 +0200

    attach recyclerview (App Works)

--- MVVM/app/src/main/java/com/shalaga44/mvvm/MainActivity.kt
+++ MVVM/app/src/main/java/com/shalaga44/mvvm/MainActivity.kt
@@ -6,16 +6,19 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.shalaga44.mvvm.databinding.ActivityMainBinding
+import com.shalaga44.mvvm.db.User
 import com.shalaga44.mvvm.db.UserDAO
 import com.shalaga44.mvvm.db.UserDatabase
 import com.shalaga44.mvvm.db.UserRepository
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private val TAG  = "BlaBlaBla"
     private lateinit var _binding: ActivityMainBinding
     private lateinit var userViewModel: UserViewModel
-
+    private var usersList : List<User> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -25,12 +28,21 @@ class MainActivity : AppCompatActivity() {
         userViewModel = ViewModelProvider(this, factory).get(UserViewModel::class.java)
         _binding.viewModel = userViewModel
         _binding.lifecycleOwner = this
+       initRecyclerView()
+
+    }
+    private fun initRecyclerView (){
+        _binding.userRecyclerview.layoutManager = LinearLayoutManager(this)
         displayUsersList()
 
     }
     private fun displayUsersList(){
         userViewModel.users.observe(this, Observer {
            Log.d(TAG, it.toString())
+//            usersList = it
+//            _binding.userRecyclerview.adapter?.notifyDataSetChanged()
+            _binding.userRecyclerview.adapter = RecyclerviewAdapter(it)
+
         })
     }
 }
