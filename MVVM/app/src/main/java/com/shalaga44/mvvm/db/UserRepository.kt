a43e65e92b25c4b47a5dbf728d4fae0508a6c8f5
shalaga44 <mohamedmoawia4@gmail.com>
2020-07-06 10:28:36 +0200

    create users repository

---
+++ MVVM/app/src/main/java/com/shalaga44/mvvm/db/UserRepository.kt
@@ -0,0 +1,18 @@
+package com.shalaga44.mvvm.db
+
+class UserRepository(val dao: UserDAO) {
+    val users = dao.getAllUsers()
+
+    suspend fun insert(user: User){
+        dao.insertUser(user)
+    }
+    suspend fun update(user: User){
+        dao.deleteUser(user)
+    }
+    suspend fun delete(user: User){
+        dao.deleteUser(user)
+    }
+    suspend fun deleteALL(){
+        dao.deleteAllUsers()
+    }
+}
