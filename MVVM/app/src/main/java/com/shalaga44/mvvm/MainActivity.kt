8bf21b873196f1af6ed05a1570b58d494188ea2e
shalaga44 <mohamedmoawia4@gmail.com>
2020-07-06 11:19:34 +0200

    added repo functions to the viewModel

--- MVVM/app/src/main/java/com/shalaga44/mvvm/MainActivity.kt
+++ MVVM/app/src/main/java/com/shalaga44/mvvm/MainActivity.kt
@@ -2,8 +2,10 @@ package com.shalaga44.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shalaga44.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ MVVM/app/src/main/java/com/shalaga44/mvvm/UserViewModel.kt
@@ -0,0 +1,62 @@
+package com.shalaga44.mvvm
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.shalaga44.mvvm.db.UserRepository
+import androidx.databinding.Bindable
+import androidx.lifecycle.viewModelScope
+import com.shalaga44.mvvm.db.User
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class UserViewModel(val repository: UserRepository) : ViewModel() {
+    val users = repository.users
+
+    @Bindable
+    val inputName = MutableLiveData<String>()
+
+    @Bindable
+    val inputEmail = MutableLiveData<String>()
+
+    @Bindable
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+
+    @Bindable
+    val clearOrDeleteButtonText = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateButtonText.value = "Save"
+        clearOrDeleteButtonText.value = "Clear All"
+    }
+
+    fun saveOrUpdate() {
+        val name: String = inputName.value!!
+        val email: String = inputEmail.value!!
+        insert(User(0, name, email))
+        inputName.value = null
+        inputEmail.value = null
+
+    }
+
+    fun clearOrDelete() {
+        clearAll()
+
+    }
+
+    fun insert(user: User): Job = viewModelScope.launch {
+        repository.insert(user)
+    }
+
+    fun update(user: User): Job = viewModelScope.launch {
+        repository.update(user)
+    }
+
+    fun dalete(user: User): Job = viewModelScope.launch {
+        repository.delete(user)
+    }
+
+    fun clearAll(): Job = viewModelScope.launch {
+        repository.deleteALL()
+    }
+
+}
